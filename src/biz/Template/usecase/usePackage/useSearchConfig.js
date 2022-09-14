0bb6c020b59104d3ed7342595eafdd7f0204ec2a
maggot_code <wowhuanhuan233@163.com>
2022-09-14 15:49:35 +0800

    Update useSearchConfig.js

--- src/biz/Template/usecase/usePackage/useSearchConfig.js
+++ src/biz/Template/usecase/usePackage/useSearchConfig.js
@@ -3,8 +3,17 @@
  * @Author: maggot-code
  * @Date: 2022-09-13 13:39:55
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-09-13 16:28:37
+ * @LastEditTime: 2022-09-14 15:48:36
  * @Description:
+ * schema.formSchema:{
+ *  inline: 是否行内表单（行内用来做搜索表单）
+ *  disabled: 是否禁用整个表单
+ *  labelWidth: 标签宽度
+ *  gutter: 表单项间隔
+ * }
+ * schema.cellSchema:[]
+ * proName:""
+ * token:""
  */
 import { unref, computed } from "@vue/composition-api";
 import { SearchConfigService } from "@/biz/Template/service/searchConfig.service";
