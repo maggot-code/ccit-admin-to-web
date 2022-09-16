9f5e9e3f888cbe5bc7b4e303cfa00125c8236a2c
maggot_code <wowhuanhuan233@163.com>
2022-09-16 13:15:37 +0800

    fix :-1:

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -3,69 +3,22 @@
  * @Author: maggot-code
  * @Date: 2022-09-14 10:05:43
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-09-16 09:55:40
+ * @LastEditTime: 2022-09-16 13:15:26
  * @Description: 
 -->
 <template>
-  <div class="biz-container">
-    <template v-for="dialog in dialogs">
-      <!-- before-close -->
-      <el-dialog
-        v-el-drag-dialog
-        v-bind="dialog.bind.value"
-        :visible.sync="dialog.visible.value"
-        :key="dialog.key"
-      >
-        <template #default>
-          <div class="biz-dialog-body">
-            <component
-              :is="dialog.toComponent"
-              :keyword="dialog.key"
-            ></component>
-          </div>
-        </template>
-
-        <template #title>
-          <div class="biz-dialog-title">
-            <p class="biz-dialog-title-content">标题渲染位置</p>
-            <div class="biz-dialog-title-control">
-              <i class="icon-ym icon-ym-nav-close" @click="dialog.release"></i>
-            </div>
-          </div>
-        </template>
-
-        <template #footer>
-          <!-- TODO -->
-        </template>
-      </el-dialog>
-    </template>
-  </div>
+  <div class="biz-tabs">tabs</div>
 </template>
 
 <script>
-import { onMounted } from "@vue/composition-api";
-import { defineDialog } from "@/biz/Dialog/usecase/defineDialog";
 export default {
   name: "Test",
   mixins: [],
   components: {},
   props: {},
   setup() {
-    const dialog = defineDialog();
-
-    onMounted(() => {
-      dialog.setupDialog();
-      dialog.setupDialog();
-      dialog.setupDialog();
-    });
-
-    return {
-      ...dialog,
-    };
+    return {};
   },
 };
 </script>
-<style lang="scss" scoped>
-@import "@bizv/style/container.scss";
-@import "@/biz/Dialog/shared/style/index.scss";
-</style>
+<style lang="scss" scoped></style>
